fcadb725557e74a1a16c6fe3789f5d6b5248bf0d
Alexandru Iacob <alexandru.iacob@dentsu.com>
2021-10-25 16:21:32 +0300

    added improvements

--- src/App.js
+++ src/App.js
@@ -2,25 +2,24 @@ import { useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 import CartProvider from './store/CartProvider';
-import Cart from './components/Cart/Cart'
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
-  function showCartHandler() {
+  const showCartHandler = () => {
     setCartIsShown(true);
-  }
+  };
 
-  function hideCartHandler() {
+  const hideCartHandler = () => {
     setCartIsShown(false);
-  }
+  };
 
   return (
     <CartProvider>
-      <Cart />
-      {cartIsShown && <div>Cart...</div>}
-      <Header onShowCart={showCartHandler}/>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
